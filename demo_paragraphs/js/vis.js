aec8bc2ed70346af606088963eeea159679870ca
Wade Fagen <waf@wirelessprvnat-172-17-209-111.near.illinois.edu>
2015-03-05 11:33:50 -0600

    graph

--- demo_paragraphs/js/vis.js
+++ demo_paragraphs/js/vis.js
@@ -4,84 +4,27 @@
 function main() {
     $.getJSON("res/graph.json",
         function (jsonData) {
-			for (var i = 0; i < jsonData.length; i++) {
-				// Add the graph
-				var freq = jsonData[i].freq;
-				var desc = jsonData[i].desc;
-				
-				var data = _.map(
-				   freq,
-				   function(value, key) {
-					  return { word: key,
-							   count: value };
-				   }
-				);
-				
-				data = _.sortBy(data,
-								function (d) {
-								   return d.count;
-								});
-
-				data.reverse()
+        
+        		var color = d3.interpolateHsl("yellow", "darkblue");
 				
 				var margin = { top: 50,
 							   left: 50,
 							   right: 50,
 							   bottom: 50 };
-				
-				// Create DOM
-				var headerDOM = document.createElement("h2");
-				var text = document.createTextNode(desc);
-				headerDOM.appendChild(text);
-				
-				var divDOM = document.createElement("div");
-				document.getElementById("content").appendChild(headerDOM);
-				document.getElementById("content").appendChild(divDOM);
-				
-				
-				
-				
+								
 				var realW = document.getElementById("content").offsetWidth;
 				
 				var width = realW - margin.left - margin.right,
-					height = 400 - margin.top - margin.bottom;
+					height = 600 - margin.top - margin.bottom;
 
-				// We will use a ordinal scale, allowing us to map a series of elements
-				// to a range (which will be the location on the x-axis of the bar
-				// graph).
-				// @see: https://github.com/mbostock/d3/wiki/Ordinal-Scales
-				//
-				// Our domain is the names of the color, as an array.
-				//   Ex: ["red", "green", "yellow", ...]
-				//
-				// To translate our data, which is an array of objects that contain
-				// both a .color and a .count, into an array of only color names,
-				// we will use _.map() to map our array into a new array.
-				//
-				// Our range is all the values in [0, width].
-				
-				var x =
-				   d3.scale.ordinal()
-						   .domain( data.map(function (d) { return d.word; } ) )
-						   .rangeBands( [0, width], 0.1 );
-						   
-				var y =
-				   d3.scale.linear()
-						   .domain( [0, d3.max( data, function(d) { return d.count; } )] )
-						   .range( [height, 0] );
-
-				var xAxis =
-				   d3.svg.axis()
-						 .scale( x )
-						 .orient("bottom");
-				
-				var yAxis =
-					d3.svg.axis()
-						  .scale(y)
-						  .orient("left");
+        
+var force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(30)
+    .size([width, height]);
 						  
 				var svg =
-				d3.select(divDOM)
+				d3.select("#content")
 				  .append("svg")
 				  .attr("w", width + margin.left + margin.right)
 				  .attr("h", height + margin.top + margin.bottom)
@@ -91,39 +34,43 @@ function main() {
 				  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
 				  ;
 				
-				var c20 = d3.scale.category20();  
-				svg.selectAll("rect")
-				  .data(data)
-				  .enter()
-				  
-				  .append("rect")
-				  .attr("x", function (d, i) { return x(d.word); } )
-				  .attr("y", function (d, i) { return y(d.count); })
-				  .attr("width", x.rangeBand() )
-				  .attr("height", function (d) { return height - y(d.count); })
-				  .style("fill", function(d, i) { return c20(i); })
-				  .style("stroke", function(d, i) { return tinycolor(c20(d,i)).darken(); })
-				  .style("stroke-width", 1)
-				  ;
-				  
-				svg.append("g")
-				  .attr("class", "axis")
-				  .call(yAxis)
-				  ;
-				  
-				svg.append("g")
-				  .attr("class", "axis")
-				  .attr("transform", "translate(0, " + height + ")")
-				  .call(xAxis)
-                  .selectAll("text")
-                  .style("text-anchor", "end")
-                  .attr("dx",-10)
-                  .attr("dy",-5)
-                  .attr("transform", "rotate(-65)")
-				  ;
+				force
+				  .nodes(jsonData.vertices)
+				  .links(jsonData.edges)
+				  .linkStrength( function (d) { if (d.wt < 0.1) return 0; return d.wt; } )
+				  .start();
+      				
+  var link = svg.selectAll(".link")
+      .data(jsonData.edges)
+      .enter()
+      .append("line")
+      .attr("class", "link")
+      .style("stroke-width",  function(d) { if (d.wt < 0.1) return 0;  return Math.sqrt((d.wt - 0.1) * 10); } ); // function(d) { return Math.sqrt(d.wt); });
+
+  var node = svg.selectAll(".node")
+      .data(jsonData.vertices)
+      .enter()
+      .append("circle")
+      .attr("class", "node")
+      .attr("r", 5)
+      .style("fill", function (d, i, a) { return color(i / jsonData.vertices.length); })
+      .call(force.drag);
+
+  node.append("title")
+      .text(function(d) { return d.text; });
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
 				  
 				
-			}
+			//}
         })
         .fail(function (d) { alert("Failed to load JSON!"); })
     ;                              
